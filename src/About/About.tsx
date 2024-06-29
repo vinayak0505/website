2d8737fffeb20e42e7f63e71e26201979fe9b7bc
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-06-29 12:56:00 +0530

    completed about

---
+++ src/About/About.tsx
@@ -0,0 +1,35 @@
+import { email, icons, info } from '../constans';
+import Styles from './About.module.scss';
+
+const About = () => {
+    return (
+        <div id='about' className={Styles.about}>
+            <div className={Styles.align}>
+                <div className={Styles.title}>Let's make something<br />amazing together.</div>
+                <div className={Styles.gap}></div>
+                <div>
+                    <span className={Styles.title}>Start by </span>
+                    <a className={Styles.link} href={'mailto:' + email.emailId + "?subject=" + email.subject} target='_blank' rel='noreferrer'>Saying Hi</a>
+                </div>
+            </div>
+            <div className={Styles.align}>
+                {
+                    info.map((item, index) => <div>
+                        <div key={index} className={Styles.heading}>{item.heading}</div>
+                        <div key={index} >{item.info}</div>
+                    </div>)
+                }
+                <div className={Styles.gap}></div>
+                <div className={Styles.icons}>
+                    {
+                        icons.map((item, index) => <a title={item.tooltip} href={item.link}>
+                            <img className={Styles.icon} key={index} src={item.icon} alt={item.tooltip} />
+                        </a>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+export default About;
